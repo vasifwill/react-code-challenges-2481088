1afd520e92cf79ca816213a13ac36890984b0f1a
Vasif Mammadov <vasif.will@gmail.com>
2023-03-10 10:11:09 -0800

    darkmood

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,23 @@
+import { useState } from "react"
+
+
 export default function DarkMode () {
+  const [color, setColor] = useState("")
+  
+  const handleBlackColor = () => {
+    setColor("#171717")
+
+  }
+
+  const handleWhiteColor = () => {
+    setColor("#fcfbfb")
+  }
+
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className='page' style={{background:color}}>
+      <button className='dark-mode-button' onClick={handleBlackColor}>Dark Mode</button>
+      <button className='light-mode-button' onClick={handleWhiteColor}>Light Mode</button>
     </div>
   )
 }
