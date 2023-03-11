29b0e0d7e308857c603fd8d4682a92efe0782fae
Vasif Mammadov <vasif.will@gmail.com>
2023-03-11 14:46:01 -0800

    form validator

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,24 +4,41 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
-  const [valid, setValid] = useState([])
+  const [message, setMessage] = useState('')
+  
 
-const handleForm  = (e) => {
-  e.preventDefault()
-  if( email.indexOf("@") == -1){
-    setValid("email is not valid")
-  } else if (password.length < 8){
-    setValid([...valid, "pasword is less than 8 character"])
-  }else if(passwordConfirm !== password){
-    setValid([...valid, "pasword is not matching"])
-  }else {
-    setValid("Form created")
+const findErrors  = () => {
+  const errors = []
+
+  if(!email || !password || !passwordConfirm){
+    errors.push("has to be placed")
   }
+    if( email.indexOf("@") == -1){
+      errors.push("email is not valid")
+    } 
+     if (password.length < 8){
+      errors.push("password has to be at leats 8 characters")
+    }
+    if(passwordConfirm !== password){
+      errors.push("paswwords has to be matched")
+    }
+
+    return errors
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const errors = findErrors()
+    setMessage(errors.length ? errors.join(', ') : "User Created")
+    
 }
+
+ 
 
   return (
     <>
-    <form onSubmit={handleForm}>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -36,9 +53,7 @@ const handleForm  = (e) => {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)} />
       <input type='submit' value='Submit' />
-      <ul>
-        {valid.map((item, index) => <li key={index}>{item}</li> )}
-      </ul>
+      <h3>{message}</h3>
     </form>
    
     </>
