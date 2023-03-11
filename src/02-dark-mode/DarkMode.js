d404c74d774a5155139e6d55c22dc4583d8df56e
Vasif Mammadov <vasif.will@gmail.com>
2023-03-11 13:16:55 -0800

    added

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -5,18 +5,8 @@ export default function DarkMode () {
 
   return (
     <div className={`page ${darkMode && 'dark-mode'}`}>
-      <button
-        className='dark-mode-button'
-        onClick={() => setDarkMode(true)}
-      >
-        Dark Mode
-      </button>
-      <button
-        className='light-mode-button'
-        onClick={() => setDarkMode(false)}
-      >
-        Light Mode
-      </button>
+      <button className='dark-mode-button' onClick={() => setDarkMode(true)}>Dark Mode</button>
+      <button className='light-mode-button' onClick={() => setDarkMode(false)}>Light Mode</button>
     </div>
   )
 }
--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,26 +4,43 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [valid, setValid] = useState([])
+
+const handleForm  = (e) => {
+  e.preventDefault()
+  if( email.indexOf("@") == -1){
+    setValid("email is not valid")
+  } else if (password.length < 8){
+    setValid([...valid, "pasword is less than 8 character"])
+  }else if(passwordConfirm !== password){
+    setValid([...valid, "pasword is not matching"])
+  }else {
+    setValid("Form created")
+  }
+}
 
   return (
-    <form>
+    <>
+    <form onSubmit={handleForm}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
         type='text' name='email'
-        onChange={e => setEmail(e.target.value)}
-      />
+        onChange={e => setEmail(e.target.value)} />
       <label htmlFor='password'>Password</label>
       <input
         type='password' name='password'
-        onChange={e => setPassword(e.target.value)}
-      />
+        onChange={e => setPassword(e.target.value)} />
       <label htmlFor='password-confirm'>Confirm Password </label>
       <input
         type='password' name='password-confirm'
-        onChange={e => setPasswordConfirm(e.target.value)}
-      />
+        onChange={e => setPasswordConfirm(e.target.value)} />
       <input type='submit' value='Submit' />
+      <ul>
+        {valid.map((item, index) => <li key={index}>{item}</li> )}
+      </ul>
     </form>
+   
+    </>
   )
 }
