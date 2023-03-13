72b56928f741cd29acd553e60a848b4a3e3c4ee8
Vasif Mammadov <vasif.will@gmail.com>
2023-03-13 13:23:43 -0700

    dogs

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function DogPics () {
+  const [dogs, setDogs] = useState('')
+  const [button, setButton] = useState(false)
   // API: https://dog.ceo/dog-api/
+  //https://dog.ceo/api/breeds/image/random
+  //"https://images.dog.ceo/breeds/bulldog-boston/n02096585_4575.jpg"
+
+  const handleClick = async() => {
+    await axios.get("https://dog.ceo/api/breeds/image/random").then((res) => {
+      setDogs(res.data.message)}).catch((error) => {
+        alert(error)
+    })
+    setButton(true)
+    
+  }
+
+  useEffect(() => {
+    handleClick()
+  }, [button])
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={ button ? dogs : "https://images.dog.ceo/breeds/bulldog-boston/n02096585_4575.jpg"} alt="dog"/>
+      <button onClick={handleClick}>🐶</button>
     </div>
   )
 }
